89ec377234d461789d69669b02af5a35975b2f53
John Laws Harrison <johnlawsharrison@gmail.com>
2019-05-24 14:36:56 -0700

    Read secret key from envvar

--- backend/playlist_backend/settings.py
+++ backend/playlist_backend/settings.py
@@ -20,7 +20,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y*kky=7q!=3zz%kal4v+7y!)dkb_*frl@0oeuq8wlz8ddyd8v7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
