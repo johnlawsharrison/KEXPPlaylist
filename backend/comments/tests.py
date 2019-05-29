b46cb2e4bfba8a4370cf3627a8ab15ed812300b6
John Laws Harrison <johnlawsharrison@gmail.com>
2019-05-29 15:23:00 -0700

    Write simple model tests

--- backend/comments/tests.py
+++ backend/comments/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from comments.models import Comment, Author, Link
+
+# Tests for Models
+class CommentModelTest(TestCase):
+
+    def test_string_representation(self):
+        comment = Comment(comment_text="Pile is a good band")
+        self.assertEqual(str(comment), comment.comment_text)
+
+# author default to KEXP?
+class AuthorModelTest(TestCase):
+
+    def test_string_representation(self):
+        author = Author(first_name="John", last_name="Harrison")
+        expected = "John Harrison"
+        self.assertEqual(str(author), expected)
+
+
+class LinkModelTest(TestCase):
+
+    def test_string_representation(self):
+        link = Link(link_url="https://pile.bandcamp.com/album/green-and-gray", play_id=2663652)
+        expected = "(https://pile.bandcamp.com/album/green-and-gray, 2663652)"
+        self.assertEqual(str(link), expected)
