48b37c4e437a776c31726d5ea4773930be4bf9ae
John Laws Harrison <johnlawsharrison@gmail.com>
2019-05-29 11:23:49 -0700

    Create serializers for models

---
+++ backend/comments/serializers.py
@@ -0,0 +1,19 @@
+from models import Comment, Author, CommentLink
+from rest_framework import serializers
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['url', 'comment_text', 'play', 'date_created', 'last_updated', 'author']
+
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['url', 'first_name', 'last_name', 'role', 'total_comments']
+
+
+class CommentLinkSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CommentLink
+        fields = ['url', 'link_url', 'comment']
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("This is the comments app, which will probably end up being a DRF app")
+from rest_framework import viewsets
+
+from serializers import CommentSerializer, AuthorSerializer, CommentLinkSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    The set of REST framework API views for the Comment model
+    """
+    queryset = Comment.objects.all().order_by('-date_created')
+    serializer_class = CommentSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    The set of REST framework API views for the Author model
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class CommentLinkViewSet(viewsets.ModelViewSet):
+    """
+    The set of REST framework API views for the CommentLink model
+    """
+    queryset = CommentLink.objects.all()
+    serializer_class = CommentLinkSerializer
