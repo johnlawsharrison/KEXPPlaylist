987bf009ecf44ecbd948c4bb7b5ddd176d373f13
John Laws Harrison <johnlawsharrison@gmail.com>
2020-07-13 16:45:32 -0700

    Update reference to remote directory

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
-git remote add deploy "${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIR}"
+git remote add deploy "${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIRECTORY}"
 git config user.name "Travis CI"
 git config user.email "johnlawsharrison+travis@gmail.com"
 
