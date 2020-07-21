f469450b25af3c20d39301706c483544a6e3e7c6
John Laws Harrison <johnlawsharrison@gmail.com>
2020-07-20 21:05:06 -0700

    Remove .env

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -xe
-# copy decrypted .env to deploy dir
-scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIRECTORY%.*}
 # push to hook
 git remote add deploy "${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIRECTORY}"
 git config user.name "Travis CI"
