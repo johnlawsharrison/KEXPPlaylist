ac948288a57acb3792268811c3196ec5621823f8
John Laws Harrison <johnlawsharrison@gmail.com>
2020-07-21 00:00:32 -0700

    Fix minor errors

--- frontend/playlist-frontend/deploy.sh
+++ frontend/playlist-frontend/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 # rsync build artifacts to deployment dir
-rsync -r --quiet --delete public ${DEPLOY_USER}@${DEPLOY_HOST}:${FRONTEND_DEPLOY_DIRECTORY}
+rsync -r --quiet --delete dist ${DEPLOY_USER}@${DEPLOY_HOST}:${FRONTEND_DEPLOY_DIRECTORY}
