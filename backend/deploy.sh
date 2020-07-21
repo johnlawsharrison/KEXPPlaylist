8e3586f3fecda0d006994cf79716bf84b614db26
John Laws Harrison <johnlawsharrison@gmail.com>
2020-07-20 20:51:33 -0700

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -xe
+# copy decrypted .env to deploy dir
+scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIRECTORY%.*}
+# push to hook
 git remote add deploy "${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIRECTORY}"
 git config user.name "Travis CI"
 git config user.email "johnlawsharrison+travis@gmail.com"
