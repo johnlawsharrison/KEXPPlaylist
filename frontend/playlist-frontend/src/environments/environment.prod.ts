70d70183fc1bbda0f41f398a8f318e230411f47b
John Laws Harrison <johnlawsharrison@gmail.com>
2020-07-21 15:38:07 -0700

    Use https

--- frontend/playlist-frontend/src/environments/environment.prod.ts
+++ frontend/playlist-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendHost: 'http://playlist.johnlawsharrison.com',
+  backendHost: 'https://playlist.johnlawsharrison.com',
   commentAPIRoot: 'api_v1',
   playlistAPIHost: 'https://legacy-api.kexp.org',
   linkRegex: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
