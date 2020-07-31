30269c02fd8038eee4b3fabbb902aa883602fc38
John Laws Harrison <johnlawsharrison@gmail.com>
2020-07-31 16:57:38 -0700

    Read from generic environment

--- frontend/playlist-frontend/src/app/playlist.service.ts
+++ frontend/playlist-frontend/src/app/playlist.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { environment } from 'src/environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -12,33 +12,10 @@ export class PlaylistService {
 
   constructor(private http: HttpClient) { }
 
-
   /**
-   * Old approach:
-   * Gets recent plays directly from the KEXP playlist API
-   * @param beginTime Date object for the begin time of the query
-   * @param endTime Date object for the end time of the query
-   */
-  getRecentPlays(beginTime: Date, endTime: Date): Observable<any> {
-    const url = `${environment.playlistAPIHost}/play/`;
-    const options = {
-      headers: new HttpHeaders({
-        // 'Authorization': 'my-auth-token'
-      }),
-      params: new HttpParams()
-      .append('begin_time', beginTime.toISOString())
-      .append('end_time', endTime.toISOString())
-    };
-    return this.http.get<any>(url, options).pipe(
-      catchError(this.handleError<any>(`getRecentPlays`))
-    );
-  }
-
-  /**
-   * Updated approach:
    * Gets recent plays by querying the backend
    * which assembles playlist rows and existing comments/links all at once
-   * NOTE: defaults to plays from the last 60 minutes
+   * defaults to plays from the last 60 minutes
    */
   getRecentPlaysFromBackend(): Observable<any> {
     const url = `${environment.backendHost}/playlist-data`;
